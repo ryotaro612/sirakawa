283cd16abe9b9b03f906062a4f951792ae774db3
Kazuma SATO <ranceworks@gmail.com>
2015-11-03 21:41:43 +0900

    support the command to create droplets, but there are some bugs. #1

--- src/digitalocean_routine/core.clj
+++ src/digitalocean_routine/core.clj
@@ -6,6 +6,20 @@
   [x]
   (println x "Hello, World!"))
 
-(defn -main []
-  (do/images)
-  )
+(defn- droplets-exist? [token]
+  "return 0 if droplets exist."
+  (not (= (count (:droplets (do/droplets token)))
+          0)))
+
+(defn- delete-droplets [] "to be implemnted")
+
+(defn- create-droplets [token number image-id]
+  (if (droplets-exist? token)
+    (delete-droplets))
+  (for [name (map #(str "rance" %) (range 0 number))]
+    (doall (do/create-droplet token nil {:name name :region "nyc3" :size "1gb" :image image-id}))
+    ))
+
+(defn -main [& args]
+  (if (= (first args) "create")
+    (create-droplets (nth args 1) (nth args 2) (nth args 3))))
