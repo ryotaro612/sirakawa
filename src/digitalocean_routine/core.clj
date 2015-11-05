c8bca2658fd546243d6f0ed29811c50801feb804
Kazuma SATO <ranceworks@gmail.com>
2015-11-05 20:45:38 +0900

    delete command support

--- src/digitalocean_routine/core.clj
+++ src/digitalocean_routine/core.clj
@@ -1,21 +1,13 @@
 (ns digitalocean-routine.core
   (:require [digitalocean.v2.core :as do]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
 
-(defn- droplets-exist? [token]
+(defn droplets-exist? [token]
   "return 0 if droplets exist."
   (not (= (count (:droplets (do/droplets token)))
           0)))
 
-(defn- delete-droplets [] "to be implemnted")
-
-(defn- create-droplets [token number image-id ssh_keys]
-  (if (droplets-exist? token)
-    (delete-droplets))
+(defn create-droplets [token number image-id ssh_keys]
   (for [name (map #(str "droplet" %) (range 0 number))]
     (doall (do/create-droplet token nil {:name name
                                          :region "nyc3"
@@ -23,6 +15,25 @@
                                          :image image-id
                                          :ssh_keys [ssh_keys]}))))
 
+(defn query-droplet-ids [token]
+  "return droplet ids as a list"
+  (let [dropletdetails (do/droplets token)]
+    (map #(:id %) (:droplets dropletdetails))))
+
+(def delete-droplet
+  "Delete a droplet by ID
+  (delete-droplet <token> dropletID) => nil (if it has been deleted successfully)"
+  (do/generic :delete :droplets))
+
+(defn delete-all-droplets [token]
+  (for [droplet-id (query-droplet-ids token)]
+    (doall (delete-droplet token droplet-id))))
+
 (defn -main [& args]
-  (if (= (first args) "create")
-    (create-droplets (nth args 1) (nth args 2) (nth args 3) (nth args 4))))
+  (cond (= (first args) "create")
+        (create-droplets (nth args 1) (nth args 2) (nth args 3) (nth args 4))
+        (= (first args) "delete")
+        (delete-all-droplets (nth args 1))
+        ))
+
+
